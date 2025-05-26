531a26fb9485ce4669f4bf3de2260f7a1c4fa59c
limcpf <daeseong0226@gmail.com>
2025-05-26 12:52:36 +0900

    feat: add .md file filter to SimpleMarkdownFinder

--- src/main/java/com/jimple/finder/SimpleMarkdownFinder.java
+++ src/main/java/com/jimple/finder/SimpleMarkdownFinder.java
@@ -14,7 +14,10 @@ public class SimpleMarkdownFinder implements MarkdownFinder{
         if(!Files.isReadable(rootDir)) throw new IllegalArgumentException("rootDir must be readable");
 
         try (Stream<Path> paths = Files.walk(rootDir)) {
-            return paths.filter(Files::isRegularFile).toList();
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(x -> x.endsWith(".md"))
+                    .toList();
         } catch (IOException e) {
             throw new RuntimeException("Error walking directory", e);
         }
