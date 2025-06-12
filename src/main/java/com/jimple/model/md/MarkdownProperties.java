bf90346652b16c9691141b79b5dae58dec7663fd
limcpf <daeseong0226@gmail.com>
2025-06-12 21:58:34 +0900

    feat: Extend `MarkdownProperties` with `description` and `thumbnailUrl` fields; add constructors and update `toString` implementation

--- src/main/java/com/jimple/model/md/MarkdownProperties.java
+++ src/main/java/com/jimple/model/md/MarkdownProperties.java
@@ -8,14 +8,27 @@ import java.time.LocalDate;
 public record MarkdownProperties(
         boolean publish,
         String title,
-        LocalDate date
+        LocalDate date,
+        String description,
+        String thumbnailUrl
 ) implements Properties {
+
+    public MarkdownProperties() {
+        this(false, "", null);
+    }
+
+    public MarkdownProperties(boolean publish, String title, LocalDate date) {
+        this(publish, title, date, "", "");
+    }
+
     @Override
     public @NotNull String toString() {
         return "MarkdownProperties{" +
                 "publish=" + publish +
                 ", title='" + title + '\'' +
                 ", date=" + date +
+                ", thumbnailUrl='" + thumbnailUrl + '\'' +
+                ", description=\n" + description +
                 '}';
     }
 }
